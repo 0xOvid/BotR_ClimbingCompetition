6fe12a803f2fdad470b9c31add5aa3b566378a50
0xOvid <117951172+0xOvid@users.noreply.github.com>
2025-07-06 21:25:15 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,10 +1,11 @@
 # Setup
 git clone https://github.com/0xOvid/BotR_ClimbingCompetition
 cd BotR_ClimbingCompetition/
-# Install required python vnen
+
 apt install python3.12-venv -y
 python3 -m venv botr
-# Run venv
+
+screen -dm bash -c "
 source botr/bin/activate
 pip install -r requirements.txt
 python -m pytest # for some reason tests fail on first run
@@ -15,4 +16,4 @@ rm ./tmptmpRoutes.csv
 rm ./tmptmpUsers.csv
 
 # Run in production
-while true; do flask run --host=0.0.0.0 -p 80; sleep 10; done
+while true; do flask run --host=0.0.0.0 -p 80; sleep 10; done"
